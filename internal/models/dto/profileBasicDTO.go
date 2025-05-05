8e6c7b95188d2959800cac1b7b1759a03be3d036
Agustin <dyagus1329@gmail.com>
2025-05-05 14:18:51 +0200

    refactor(models/dto, db): remove UserID from ProfileBasicDTO and queries
    The UserID field was removed from the ProfileBasicDTO struct and the corresponding SQL queries in the FollowRepository. This change simplifies the data model by eliminating an unused field, improving maintainability and reducing unnecessary data retrieval.

--- internal/models/dto/profileBasicDTO.go
+++ internal/models/dto/profileBasicDTO.go
@@ -1,7 +1,6 @@
 package dto
 
 type ProfileBasicDTO struct {
-	UserID         uint    `json:"userId"`
 	ProfileID      uint    `json:"profileId"`
 	Username       string  `json:"username"`
 	ProfilePicture *string `json:"profilePicture,omitempty"`
