27c3e5ad706e970ae06f0a47fecc4d4d19429a1d
Agustin <dyagus1329@gmail.com>
2025-05-05 13:59:58 +0200

    refactor:  add UserID to ProfileBasicDTO
    - Add UserID field to ProfileBasicDTO to include user identification in the profile data

--- internal/models/dto/profileBasicDTO.go
+++ internal/models/dto/profileBasicDTO.go
@@ -1,6 +1,7 @@
 package dto
 
 type ProfileBasicDTO struct {
+	UserID         uint    `json:"userId"`
 	ProfileID      uint    `json:"profileId"`
 	Username       string  `json:"username"`
 	ProfilePicture *string `json:"profilePicture,omitempty"`
